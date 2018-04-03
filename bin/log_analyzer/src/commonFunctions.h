508b82d982e7dd444e3797aec34f03a9792ce5bd
eseuteo <ricardoholthausen@gmail.com>
2018-04-03 16:49:52 +0200

    Delete commonFunctions.h

--- bin/log_analyzer/src/commonFunctions.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMON_FUNCTIONS_H
-#define COMMON_FUNCTIONS_H
-#include "structs.h"
-
-void terror(char *s);
-char buffered_fgetc(char *buffer, uint64_t *pos, uint64_t *read, FILE *f);
-
-#endif /* COMMON_FUNCTIONS_H */
